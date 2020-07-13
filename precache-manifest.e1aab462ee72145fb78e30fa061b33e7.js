535d314de099bde2e811474d656fce5a7a121689
Rose Cooper <rosecooper0@gmail.com>
2020-07-13 10:19:32 -0400

    Updates

---
+++ precache-manifest.e1aab462ee72145fb78e30fa061b33e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9e4d7ec50316478b9a1fece164483e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a397956f60b207cab31",
+    "url": "/static/css/main.402e5021.chunk.css"
+  },
+  {
+    "revision": "42b08344071e8cce3af6",
+    "url": "/static/js/2.f2fa5f31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2fa5f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a397956f60b207cab31",
+    "url": "/static/js/main.e632c1aa.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  }
+]);
