cb9d4daadb445c9cb73df17456aaa880a0de5f4a
Rose Cooper <rosecooper0@gmail.com>
2020-09-14 10:14:50 -0400

    typo fix

--- src/Components/Research/ResBrain.js
+++ src/Components/Research/ResBrain.js
@@ -13,7 +13,7 @@ const Container = styled.div`
 `;
 
 const ResBrain = ({mobile,bigScreen,portrait}) => {
-  var textString = `Remembering the past engages a large scale network of brain regions, often referred to as the default network. Using functional magnetic resonance imaging (fMRI), my work tests how information flows through this network and how its activity and connections support our ability to perceive and recall unique events. For example, we have shown that connections to and within a posterior medial subsystem support the amount of detail we recall about an event, with distinct connections supporting memory for objects and locations within the same event. I am particularly interested in how the pattern of functional connections with the default network explains memory “fingerprints” and differences in the way memories are experienced over time and between different people. `;
+  var textString = `Remembering the past engages a large scale network of brain regions, often referred to as the default network. Using functional magnetic resonance imaging (fMRI), my work tests how information flows through this network and how its activity and connections support our ability to perceive and recall unique events. For example, we have shown that connections to and within a posterior medial subsystem support the amount of detail we recall about an event, with distinct connections supporting memory for objects and locations within the same event. I am particularly interested in how the pattern of functional connections within the default network explains memory “fingerprints” and differences in the way memories are experienced over time and between different people.`;
 
   const imageSource = "./img/brain.png";
 
